264e3b718ae0f81c1b8eea75afda3650e3208614
Bryan13092005 <bryan.salvador@epn.edu.ec>
2025-12-13 10:22:40 -0500

    Base de Datos Script

---
+++ ScriptBaseDatos.sql
@@ -0,0 +1,11 @@
+create database SistemaBancario;
+use SistemaBancario;
+create table cuentasUsuario(
+id int primary key auto_increment,
+usuario varchar(50) not null,
+clave varchar(50) not null,
+monto decimal not null check(monto>0),
+historialTransacciones varchar(200) default 'Ninguna'
+);
+
+insert into cuentasUsuario(usuario,clave,monto) values('root','admin123',1000.00);
