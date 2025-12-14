b8e245897096b5adb6a3972907a7b06bbf27488f
Bryan13092005 <bryan.salvador@epn.edu.ec>
2025-12-13 22:05:27 -0500

    Utilizacion base de datos

--- ScriptBaseDatos.sql
+++ ScriptBaseDatos.sql
@@ -2,10 +2,11 @@ create database SistemaBancario;
 use SistemaBancario;
 create table cuentasUsuario(
 id int primary key auto_increment,
-usuario varchar(50) not null,
+usuario varchar(50) not null unique,
 clave varchar(50) not null,
-monto decimal not null check(monto>0),
+monto decimal not null check(monto>=0),
 historialTransacciones varchar(200) default 'Ninguna'
 );
 
 insert into cuentasUsuario(usuario,clave,monto) values('root','admin123',1000.00);
+
