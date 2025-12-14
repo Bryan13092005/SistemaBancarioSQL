3d3ef501a063671e281e2c407dabd981f12a2d7d
Bryan13092005 <bryan.salvador@epn.edu.ec>
2025-12-13 23:05:14 -0500

    Update SQL script

--- ScriptBaseDatos.sql
+++ ScriptBaseDatos.sql
@@ -5,8 +5,7 @@ id int primary key auto_increment,
 usuario varchar(50) not null unique,
 clave varchar(50) not null,
 monto decimal not null check(monto>=0),
-historialTransacciones varchar(200) default 'Ninguna'
+historialTransacciones varchar(2000) default 'Ninguna'
 );
 
 insert into cuentasUsuario(usuario,clave,monto) values('root','admin123',1000.00);
-
